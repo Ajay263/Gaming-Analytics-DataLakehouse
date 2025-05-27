2473d6a80ee3a21d476ca572b662a6170f72d4f8
Ajay263 <junioralexio607@gmail.com>
2025-05-27 13:38:17 +0200

    Fix:Updated the region

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,11 +9,11 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "gamepulse-tf-backend-resources"
-    key            = "terraform.tfstate"
-    region         = "eu-west-3"
-    encrypt        = true
-    dynamodb_table = "gamepulse-terraform-state-lock"
+    bucket       = "gamepulse-tf-backend-resources"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
 
